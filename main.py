82cafef511385d0635d7c844b1d8b048087788e3
Ender50 <amjadsharafi10@gmail.com>
2018-03-10 16:15:00 +0300

    using a simple discord bot script to understand the library.

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from credentials import cred
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        print(message.channel)
+        print(message)
+        print(message.author)
+        # print(client.logs_from(message.channel))
+        # print(message)
+
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+            print(log)
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await client.send_message(message.channel, 'Done sleeping')
+
+
+client.run(cred['bot_token'])
