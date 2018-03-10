112d2d2b7a4ef9780217e1ecc3bc705c399025f0
Ender50 <amjadsharafi10@gmail.com>
2018-03-10 17:35:21 +0300

    removing everything that has anything with database module.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
 import discord
-from firebase_admin.db import TransactionError
-from Database import database
 from credentials import cred
 from games.game_2048 import Game2048
 
 client = discord.Client()
-db = database.Database(cred['cert'],
-                       cred['database_url'],
-                       app_name="bot-test"
-                       )
 games = {}
 
 
@@ -32,15 +26,7 @@ async def on_message(message):
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
         await client.add_reaction(tmp, emoji="➡")
         respond = await client.wait_for_reaction(message=tmp, check=lambda r, u: u != client.user)
-        try:
-            db.get_reference(
-                "reactions/users/{}/number".format(respond.user.name + respond.user.id)) \
-                .transaction(lambda value: value + 1 if value else 1)
-        except TransactionError:
-            print("transaction failed!")
 
-        await db.write("reactions/users/{0}".format(respond.user.name + respond.user.id),
-                       {'emoji': respond.reaction.emoji}, update=True)
         await client.send_message(message.channel,
                                   "{0.user} reacted with {0.reaction.emoji}".format(respond))
     elif message.content.startswith('!game'):
